82a97e1127d7c78e45b7c90113cb04f08883407d
唐百万 <tangbaiwan1597@gmail.com>
2024-03-07 09:45:08 +0800

    add

--- Source.cpp
+++ Source.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 #include <fstream>
 #include <Windows.h>
-
-const int SAMPLE_RATE = 44100;  // 采样率
+#include <Shlwapi.h>
+const int SAMPLE_RATE = 11025;  // 采样率
 const int NUM_SECONDS = 5;     // 音频时长（秒）
-const int NUM_CHANNELS = 1;    // 单声道
+const int NUM_CHANNELS = 2;    // 单声道
 const int BITS_PER_SAMPLE = 16; // 每个样本的位数
 
+#pragma comment(lib, "shlwapi.lib")  //Windows API   PathFileExists
 void GenerateRandomAudio(short* buffer, int numSamples)
 {
 	for (int i = 0; i < numSamples; i++) {
@@ -16,32 +17,37 @@ void GenerateRandomAudio(short* buffer, int numSamples)
 
 int main()
 {
+	std::string dbgInfo;
+	dbgInfo= "md5StrUp";
+	OutputDebugStringA(dbgInfo.c_str());
+	OutputDebugStringA("dsadsss");
+	OutputDebugStringW(L"sdasdad");
 	// 打开.wav文件
-	std::ofstream file("output.wav", std::ios::binary);
+	//std::ofstream file("bxs_output.wav", std::ios::binary);
 
-	// WAV文件头
-	WAVEFORMATEX waveFormat;
-	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
-	waveFormat.nChannels = NUM_CHANNELS;
-	waveFormat.nSamplesPerSec = SAMPLE_RATE;
-	waveFormat.nAvgBytesPerSec = SAMPLE_RATE * NUM_CHANNELS * BITS_PER_SAMPLE / 8;
-	waveFormat.nBlockAlign = NUM_CHANNELS * BITS_PER_SAMPLE / 8;
-	waveFormat.wBitsPerSample = BITS_PER_SAMPLE;
-	waveFormat.cbSize = 0;
+	//// WAV文件头
+	//WAVEFORMATEX waveFormat;
+	//waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	//waveFormat.nChannels = NUM_CHANNELS;
+	//waveFormat.nSamplesPerSec = SAMPLE_RATE;
+	//waveFormat.nAvgBytesPerSec = SAMPLE_RATE * NUM_CHANNELS * BITS_PER_SAMPLE / 8;
+	//waveFormat.nBlockAlign = NUM_CHANNELS * BITS_PER_SAMPLE / 8;
+	//waveFormat.wBitsPerSample = BITS_PER_SAMPLE;
+	//waveFormat.cbSize = 0;
 
-	file.write(reinterpret_cast<const char*>(&waveFormat), sizeof(waveFormat));
+	//file.write(reinterpret_cast<const char*>(&waveFormat), sizeof(waveFormat));
 
-	// 生成音频数据
-	int numSamples = SAMPLE_RATE * NUM_SECONDS;
-	short* audioBuffer = new short[numSamples];
-	GenerateRandomAudio(audioBuffer, numSamples);
+	//// 生成音频数据
+	//int numSamples = SAMPLE_RATE * NUM_SECONDS;
+	//short* audioBuffer = new short[numSamples];
+	//GenerateRandomAudio(audioBuffer, numSamples);
 
-	// 写入音频数据到文件
-	file.write(reinterpret_cast<const char*>(audioBuffer), numSamples * sizeof(short));
+	//// 写入音频数据到文件
+	//file.write(reinterpret_cast<const char*>(audioBuffer), numSamples * sizeof(short));
 
-	file.close();
+	//file.close();
 
-	delete[] audioBuffer;
+	//delete[] audioBuffer;
 
 	std::cout << "Audio generation and file writing complete." << std::endl;
 
